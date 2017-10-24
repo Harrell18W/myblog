5d80a39288d76872102de7e0e991c34564d3c50e
William Harrell <wharrell1@me.com>
2017-10-24 08:48:07 -0400

    added js to populate page with posts from posts.json

--- website/main.js
+++ website/main.js
@@ -1,7 +1,10 @@
 // JavaScript Document
 
-function init() {
+var posts = 0;
+
+window.onload = function() {
 	loadMain();
+	loadPosts();
 }
 
 function loadMain() {
@@ -14,6 +17,26 @@ function loadMain() {
 		document.getElementById('title').innerHTML = data.title;
 		document.getElementById('motd').innerHTML = data.motd;
 		document.getElementById('body').style.backgroundColor = data.bgColor;
+		posts = data.posts;
+	};
+	request.send();
+	
+}
+
+function loadPosts() {
+	
+	var request = new XMLHttpRequest();
+	request.open('GET', 'posts.json');
+	request.overrideMimeType('application/json');
+	request.onload = function() {
+		var data = JSON.parse(request.responseText);
+		for(var x = 0; x < posts; x++) {
+			document.getElementById('posts').innerHTML = '<div class="post">\
+							<h3>' + data[x].title + '</h3>\
+							<h6>' + data[x].timestamp + '</h6>\
+							<p>' + data[x].content + '</p>\
+							</div>' + document.getElementById('posts').innerHTML;
+		}
 	};
 	request.send();
 	
